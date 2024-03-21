2736b6396c6fad89b33669a3b1678f7aebe9a33a
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-03-21 11:22:20 +0100

    hardcoded room

--- src/client.ts
+++ src/client.ts
@@ -20,7 +20,7 @@ function add(text: string) {
 // It handles reconnection logic, buffering messages while it's offline, and more.
 const conn = new PartySocket({
   host: PARTYKIT_HOST,
-  room: PARTYKIT_ROOM,
+  room: "little-fern-room",
 });
 
 // You can even start sending messages before the connection is open!
