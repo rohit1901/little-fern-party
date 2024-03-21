a4c6d87566b211a9a2ce70bd3df996924a64155a
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-03-21 21:36:27 +0100

    onRequest handler

--- src/client.ts
+++ src/client.ts
@@ -12,30 +12,30 @@ const output = document.getElementById("app") as HTMLDivElement;
 
 // Helper function to add a new line to the DOM
 function add(text: string) {
-  output.appendChild(document.createTextNode(text));
-  output.appendChild(document.createElement("br"));
+    output.appendChild(document.createTextNode(text));
+    output.appendChild(document.createElement("br"));
 }
 
 // A PartySocket is like a WebSocket, except it's a bit more magical.
 // It handles reconnection logic, buffering messages while it's offline, and more.
 const conn = new PartySocket({
-  host: PARTYKIT_HOST,
-  room: "little-fern-room",
+    host: PARTYKIT_HOST,
+    room: "little-fern-room",
 });
 
 // You can even start sending messages before the connection is open!
 conn.addEventListener("message", (event) => {
-  add(`Received -> ${event.data}`);
+    add(`Received -> ${event.data}`);
 });
 
 // Let's listen for when the connection opens
 // And send a ping every 2 seconds right after
 conn.addEventListener("open", () => {
-  add("Connected!");
-  add("Sending a ping every 2 seconds...");
-  // TODO: make this more interesting / nice
-  clearInterval(pingInterval);
-  pingInterval = setInterval(() => {
-    conn.send("ping");
-  }, 1000);
+    add("Connected!");
+    add("Sending a ping every 2 seconds...");
+    // TODO: make this more interesting / nice
+    clearInterval(pingInterval);
+    pingInterval = setInterval(() => {
+        conn.send("ping");
+    }, 1000);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,65 @@
 import type * as Party from "partykit/server";
+import type {PartyKitRoom} from "partykit/server";
+
 type LFPartyNotification = {
-  type: "notification" | "acknowledgement";
-  notification?: {
-    message: string;
-    read: boolean;
-    dateCreated: Date;
-  }
+    type: "notification" | "acknowledgement";
+    notification?: {
+        message: string;
+        read: boolean;
+        dateCreated: Date;
+    }
 }
 export default class Server implements Party.Server {
-  constructor(readonly room: Party.Room) {}
+    constructor(readonly room: Party.Room) {
+    }
 
-  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    // A websocket just connected!
-    console.log(
-      `Connected:
+    onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
+        // A websocket just connected!
+        console.log(
+            `Connected:
   id: ${conn.id}
   room: ${this.room.id}
   url: ${new URL(ctx.request.url).pathname}`
-    );
+        );
 
-    // let's send a message to the connection
-    conn.send(JSON.stringify({
-      type: "acknowledgement",
-    }));
-  }
+        // let's send a message to the connection
+        conn.send(JSON.stringify({
+            type: "acknowledgement",
+        }));
+    }
 
-  onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    this.room.broadcast(
-        JSON.stringify({
-            type: "notification",
-            notification: {
-                message,
-                read: false,
-                dateCreated: new Date(),
-            }
-        }),
-      // ...except for the connection it came from
-      [sender.id]
-    );
-  }
+    onMessage(message: string, sender: Party.Connection) {
+        // let's log the message
+        console.log(`connection ${sender.id} sent message: ${message}`);
+        // as well as broadcast it to all the other connections in the room...
+        this.room.broadcast(
+            JSON.stringify({
+                type: "notification",
+                notification: {
+                    message,
+                    read: false,
+                    dateCreated: new Date(),
+                }
+            }),
+            // ...except for the connection it came from
+            [sender.id]
+        );
+    }
+    async onRequest(request: Party.Request) {
+        // push new message
+        if (request.method === "POST") {
+            const payload = await request.json() as LFPartyNotification;
+            const partyNotification = JSON.stringify({
+                type: "notification",
+                notification: payload.notification
+            })
+            console.log("Received notification:", partyNotification);
+            this.room.broadcast(partyNotification);
+            return new Response("OK", { status: 200 });
+        }
+
+        return new Response("Method not allowed", { status: 405 });
+    }
 }
 
 Server satisfies Party.Worker;
