70bf6201114e7276682a52877f7388289e7420e7
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-03-21 12:03:38 +0100

    notification type

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import type * as Party from "partykit/server";
-
+type LFPartyNotification = {
+  type: "notification" | "acknowledgement";
+  notification?: {
+    message: string;
+    read: boolean;
+    dateCreated: Date;
+  }
+}
 export default class Server implements Party.Server {
   constructor(readonly room: Party.Room) {}
 
@@ -13,7 +20,9 @@ export default class Server implements Party.Server {
     );
 
     // let's send a message to the connection
-    conn.send("hello from server");
+    conn.send(JSON.stringify({
+      type: "acknowledgement",
+    }));
   }
 
   onMessage(message: string, sender: Party.Connection) {
@@ -21,7 +30,14 @@ export default class Server implements Party.Server {
     console.log(`connection ${sender.id} sent message: ${message}`);
     // as well as broadcast it to all the other connections in the room...
     this.room.broadcast(
-      `${sender.id}: ${message}`,
+        JSON.stringify({
+            type: "notification",
+            notification: {
+                message,
+                read: false,
+                dateCreated: new Date(),
+            }
+        }),
       // ...except for the connection it came from
       [sender.id]
     );
