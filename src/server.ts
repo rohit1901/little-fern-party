d2dc0b5f175f415cef15382fd49e56492090766c
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-04-01 14:45:40 +0200

    error handling

--- src/server.ts
+++ src/server.ts
@@ -35,68 +35,81 @@ export default class Server implements Party.Server {
       return req;
     } catch (e) {
       console.error(e);
-      return new Response("Unauthorized", { status: 401 });
+      return new Response("Internal Server Error", { status: 500 });
     }
   }
 
   onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    // A websocket just connected!
-    console.info(
-      `Connected: id: ${conn.id}, room: ${this.room.id}, url: ${new URL(ctx.request.url).pathname}`,
-    );
+    try {
+      // A websocket just connected!
+      console.info(
+        `Connected: id: ${conn.id}, room: ${this.room.id}, url: ${new URL(ctx.request.url).pathname}`,
+      );
 
-    // let's send a message to the connection
-    conn.send(
-      JSON.stringify({
-        type: "acknowledgement",
-      }),
-    );
+      // let's send a message to the connection
+      conn.send(
+        JSON.stringify({
+          type: "acknowledgement",
+        }),
+      );
+    } catch (e) {
+      console.error("Internal Server Error", e);
+    }
   }
 
   onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.info(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    this.room.broadcast(
-      JSON.stringify({
-        type: "notification",
-        notification: {
-          message,
-          read: false,
-          dateCreated: new Date(),
-        },
-      }),
-      // ...except for the connection it came from
-      [sender.id],
-    );
+    try {
+      // let's log the message
+      console.info(`connection ${sender.id} sent message: ${message}`);
+      // as well as broadcast it to all the other connections in the room...
+      this.room.broadcast(
+        JSON.stringify({
+          type: "notification",
+          notification: {
+            message,
+            read: false,
+            dateCreated: new Date(),
+          },
+        }),
+        // ...except for the connection it came from
+        [sender.id],
+      );
+    } catch (e) {
+      console.error("Internal Server Error", e);
+    }
   }
 
   async onRequest(request: Party.Request) {
-    // push new message
-    if (request.method === "POST") {
-      const payload: LFPartyNotification & {
-        token?: string;
-      } = await request.json();
-      if (!payload.token)
-        return new Response("Missing hCaptcha token", { status: 400 });
-      const data = await hCaptchaVerify(
-        process.env.NEXT_PUBLIC_HCAPTCHA_SECRET,
-        payload.token,
-      );
-      if (data.success) {
-        console.info("Successfully verified hCaptcha token");
-        const partyNotification = JSON.stringify({
-          type: "notification",
-          notification: payload.notification,
-        });
-        console.info("Received notification:", partyNotification);
-        this.room.broadcast(partyNotification);
-        return new Response("OK", { status: 200 });
+    try {
+      // push new message
+      if (request.method === "POST") {
+        const payload: LFPartyNotification & {
+          token?: string;
+        } = await request.json();
+        if (!payload.token)
+          return new Response("Missing hCaptcha token", { status: 400 });
+        const data = await hCaptchaVerify(
+          process.env.NEXT_PUBLIC_HCAPTCHA_SECRET,
+          payload.token,
+        );
+        if (data.success) {
+          console.info("Successfully verified hCaptcha token");
+          const partyNotification = JSON.stringify({
+            type: "notification",
+            notification: payload.notification,
+          });
+          console.info("Received notification:", partyNotification);
+          this.room.broadcast(partyNotification);
+          return new Response("OK", { status: 200 });
+        }
+        console.error("Failed to verify hCaptcha token", data);
+        return new Response("Invalid hCaptcha token", { status: 400 });
       }
-      console.error("Failed to verify hCaptcha token", data);
-      return new Response("Failed to verify hCaptcha token", { status: 400 });
+      return new Response("Invalid request", { status: 400 });
+    } catch (e) {
+      console.error(e);
+      return new Response("Internal Server Error", { status: 500 });
     }
-    return new Response("Invalid request", { status: 400 });
   }
 }
 // @eslint-ignore-next-line
