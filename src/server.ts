be95552692c55eccfef6611096ff4b9288a38200
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-04-01 14:14:34 +0200

    Authenticated user info logged

--- src/server.ts
+++ src/server.ts
@@ -1,95 +1,103 @@
-import {verify as hCaptchaVerify} from "./hcaptcha";
+import { verify as hCaptchaVerify } from "./hcaptcha";
 import type * as Party from "partykit/server";
-import * as jose from 'jose'
+import * as jose from "jose";
 
 type LFPartyNotification = {
-    type: "notification" | "acknowledgement";
-    notification?: {
-        message: string;
-        read: boolean;
-        dateCreated: Date;
-    }
-}
+  type: "notification" | "acknowledgement";
+  notification?: {
+    message: string;
+    read: boolean;
+    dateCreated: Date;
+  };
+};
 
 export default class Server implements Party.Server {
-    constructor(readonly room: Party.Room) {
+  constructor(readonly room: Party.Room) {}
+
+  static async onBeforeConnect(req: Party.Request) {
+    // Check if the request is authorized
+    try {
+      const token = new URL(req.url).searchParams.get("token");
+      if (!token) {
+        return new Response("Unauthorized", { status: 401 });
+      }
+      const JWKS = jose.createRemoteJWKSet(
+        new URL(
+          `${process.env.NEXT_PUBLIC_AUTH0_ISSUER}/.well-known/jwks.json`,
+        ),
+      );
+
+      const { payload: user } = await jose.jwtVerify(token, JWKS, {
+        issuer: `${process.env.NEXT_PUBLIC_AUTH0_ISSUER}/`,
+      });
+      console.info(`User ${user.sub} authenticated`);
+      // forward the request onwards on onConnect
+      return req;
+    } catch (e) {
+      console.error(e);
+      return new Response("Unauthorized", { status: 401 });
     }
+  }
 
-    static async onBeforeConnect(req: Party.Request) {
-        // Check if the request is authorized
-        try {
-            const token = new URL(req.url).searchParams.get("token");
-            if (!token) {
-                return new Response("Unauthorized", {status: 401});
-            }
-            const JWKS = jose.createRemoteJWKSet(
-                new URL(`${process.env.NEXT_PUBLIC_AUTH0_ISSUER}/.well-known/jwks.json`)
-            );
+  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
+    // A websocket just connected!
+    console.info(
+      `Connected: id: ${conn.id}, room: ${this.room.id}, url: ${new URL(ctx.request.url).pathname}`,
+    );
 
-            const {payload: user} = await jose.jwtVerify(token, JWKS, {
-                issuer: `${process.env.NEXT_PUBLIC_AUTH0_ISSUER}/`,
-            });
-            // forward the request onwards on onConnect
-            return req;
-        } catch (e) {
-            console.error(e);
-            return new Response("Unauthorized", {status: 401});
-        }
+    // let's send a message to the connection
+    conn.send(
+      JSON.stringify({
+        type: "acknowledgement",
+      }),
+    );
+  }
+
+  onMessage(message: string, sender: Party.Connection) {
+    // let's log the message
+    console.info(`connection ${sender.id} sent message: ${message}`);
+    // as well as broadcast it to all the other connections in the room...
+    this.room.broadcast(
+      JSON.stringify({
+        type: "notification",
+        notification: {
+          message,
+          read: false,
+          dateCreated: new Date(),
+        },
+      }),
+      // ...except for the connection it came from
+      [sender.id],
+    );
+  }
+
+  async onRequest(request: Party.Request) {
+    // push new message
+    if (request.method === "POST") {
+      const payload: LFPartyNotification & {
+        token?: string;
+      } = await request.json();
+      if (!payload.token)
+        return new Response("Missing hCaptcha token", { status: 400 });
+      const data = await hCaptchaVerify(
+        process.env.NEXT_PUBLIC_HCAPTCHA_SECRET,
+        payload.token,
+      );
+      if (data.success) {
+        console.info("Successfully verified hCaptcha token");
+        const partyNotification = JSON.stringify({
+          type: "notification",
+          notification: payload.notification,
+        });
+        console.info("Received notification:", partyNotification);
+        this.room.broadcast(partyNotification);
+        return new Response("OK", { status: 200 });
+      }
+      console.error("Failed to verify hCaptcha token", data);
+      return new Response("Failed to verify hCaptcha token", { status: 400 });
     }
-
-    onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-        // A websocket just connected!
-        console.info(
-            `Connected: id: ${conn.id}, room: ${this.room.id}, url: ${new URL(ctx.request.url).pathname}`
-        );
-
-        // let's send a message to the connection
-        conn.send(JSON.stringify({
-            type: "acknowledgement",
-        }));
-    }
-
-    onMessage(message: string, sender: Party.Connection) {
-        // let's log the message
-        console.info(`connection ${sender.id} sent message: ${message}`);
-        // as well as broadcast it to all the other connections in the room...
-        this.room.broadcast(
-            JSON.stringify({
-                type: "notification",
-                notification: {
-                    message,
-                    read: false,
-                    dateCreated: new Date(),
-                }
-            }),
-            // ...except for the connection it came from
-            [sender.id]
-        );
-    }
-
-    async onRequest(request: Party.Request) {
-        // push new message
-        if (request.method === "POST") {
-            const payload: LFPartyNotification & {
-                token?: string;
-            } = await request.json()
-            if (!payload.token) return new Response("Missing hCaptcha token", {status: 400});
-            const data = await hCaptchaVerify(process.env.NEXT_PUBLIC_HCAPTCHA_SECRET, payload.token)
-            if (data.success) {
-                console.info("Successfully verified hCaptcha token");
-                const partyNotification = JSON.stringify({
-                    type: "notification",
-                    notification: payload.notification
-                })
-                console.info("Received notification:", partyNotification);
-                this.room.broadcast(partyNotification);
-                return new Response("OK", {status: 200});
-            }
-            console.error("Failed to verify hCaptcha token", data);
-            return new Response("Failed to verify hCaptcha token", {status: 400});
-        }
-        return new Response("Invalid request", {status: 400});
-    }
+    return new Response("Invalid request", { status: 400 });
+  }
 }
 // @eslint-ignore-next-line
 // noinspection BadExpressionStatementJS
