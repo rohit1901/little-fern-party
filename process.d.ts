6438b70b1ab6cd7c951624210d1e4927185a0c11
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-04-05 11:02:25 +0200

    CHORE: updated process.d

--- process.d.ts
+++ process.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
     export interface ProcessEnv {
         // Auth0
         NEXT_PUBLIC_AUTH0_ISSUER: string
+        NEXT_PUBLIC_AUTH0_ISSUER_DEV: string
         // HCaptcha
         NEXT_PUBLIC_HCAPTCHA_SECRET: string
         NEXT_PUBLIC_HCAPTCHA_VERIFY_URL: string
